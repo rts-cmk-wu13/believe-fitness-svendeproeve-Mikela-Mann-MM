b1e5f4eeba231fa11e45061cfb669cd7d3d45ac3
mikelamann1@gmail.com <mikelamann1@gmail.com>
2026-03-09 11:57:48 +0100

    next config opdateret

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
