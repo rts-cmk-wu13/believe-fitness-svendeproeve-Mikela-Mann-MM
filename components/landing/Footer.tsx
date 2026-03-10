05a4dc1c46fc3ea957bd87164c65e26ad700878c
mikelamann1@gmail.com <mikelamann1@gmail.com>
2026-03-10 11:44:47 +0100

    footer opdateret

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -1,21 +1,28 @@
-/* 
+
 
 export default function Footer() {
   return (
-    <footer className="bg-(--brand-dark) text-white px-6 py-8 text-center">
-  <div className="flex justify-center mb-2 pb-4">
-    <img
-      src="/logo/LD-logo.png"
-      alt="Landrup Dans logo"
-      className="w-10 h-10 object-contain"
-    />
-  </div>
-  <p className="font-display font-bold text-sm mb-1">Landrup Dans</p>
-  <p className="text-brand-muted text-xs">
-    Pulsen 8 · 4000 Roskilde
-    <br />
-    Tlf. 3540 4550
-  </p>
-</footer>
+    <footer className="content-wrapper py-10 text-center border-t border-(--grey-border)">
+      {/* "Logo" */}
+
+
+      <p className="text-2xl font-black leading-tight mb-1 text-(--barnd-yellow)">
+        Believe Fitness
+      </p>
+      <p className="text-sm font-bold mb-6 text-(--brand-black)">
+        Train like a pro
+      </p>
+
+      {/* Contact info */}
+
+      <div className="flex flex-col gap-1">
+        <p className="text-sm text-(--grey-dark)">
+          Rabalderstræde 48 · 4000 Roskilde
+        </p>
+        <p className="text-sm text-(--grey-dark)">
+          hello@believe-fitness.com
+        </p>
+      </div>
+    </footer>
   );
-} */
+} 
--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 import { getSession } from "@/lib/session";
 
 export default async function Hero() {
-const session = await getSession();
+    const session = await getSession();
 
     return (
         <section className="relative content-wrapper py10 bg-(--white)">
             <h1 className="text-4xl font-black leading-tight mb-1 text-(--brand-yellow)">
-            Welcome to <br />
-             Believe Fitness
-             </h1>
-             <div className="flex gap-3 mt-6">
+                Welcome to <br />
+                Believe Fitness
+            </h1>
+            <div className="flex gap-3 mt-6">
                 <Link href="/classes" className="btn-primary w-auto px-6">
-                CLASSES
+                    CLASSES
                 </Link>
                 {session ? (
-                  <Link href="/profile" className="btn-primary w-auto px-6">
-                    MY PROFILE
-                  </Link>
+                    <Link href="/profile" className="btn-primary w-auto px-6">
+                        MY PROFILE
+                    </Link>
                 ) : (
-                  <Link href="/login" className="btn-secondary w-auto px-6">
-                    LOG IN
-                  </Link>
+                    <Link href="/login" className="btn-secondary w-auto px-6">
+                        LOG IN
+                    </Link>
                 )}
-             </div>
+            </div>
         </section>
     );
 } 
